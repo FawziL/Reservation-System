0baf0f7ad9f9b7740ed3089aba9084c7e5a02915
FawziL <fawzi.jlr@gmail.com>
2024-09-16 13:20:45 -0400

    Created users, reservation and auth modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ReservationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
