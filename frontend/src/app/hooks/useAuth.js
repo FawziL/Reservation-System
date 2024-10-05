d54fdba6d6bcf04694acca5f4de2ad5443a8e408
FawziL <fawzi.jlr@gmail.com>
2024-10-04 23:55:53 -0400

    Modified import router, added NavBar and changed the home

--- frontend/src/app/hooks/useAuth.js
+++ frontend/src/app/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export const useAuth = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
